2fd8dae282556f8579199a4034431f437c2f91d6
pukuan.cheng <pukuan.cheng@mail.utoronto.ca>
2024-10-21 20:20:38 -0400

    ask to input player name and check if finish is input

--- timer.py
+++ timer.py
@@ -9,4 +9,10 @@ sit_down_list = []
 print(f"You have between 10 and 25 seconds to sit down.")
 print("Enter the names of players who sit down one by one.")
 print("When no more players will sit down, type 'finish'.")
+start_time=time.time()
 
+while time.time() - start_time < sleep_time:
+    player = input("Enter player's name who sat down (or type 'finish' to stop): ")
+    if player.lower() == 'finish':
+        break
+    sit_down_list.append(player)
